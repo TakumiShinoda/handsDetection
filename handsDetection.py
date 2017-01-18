618dcfa23c60a6c8324dd914e66eb54bdb09eaeb
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-01-18 19:00:51 +0900

    add color detect

--- handsDetection.py
+++ handsDetection.py
@@ -1,5 +1,6 @@
 import cv2
 
+lastRect = []
 cascade_path = "/Users/macuser/Documents/Python/openCV/handsDetection/xml/cascade.xml"
 cascade = cv2.CascadeClassifier(cascade_path)
 color = (255, 255, 255)
@@ -14,8 +15,20 @@ while True:
     image_gray = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
     rect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(minsize,minsize))
 
-    if len(rect) > 0:
-        for handRect in rect:
+    for colorRect in rect:
+        cx = colorRect[0] + (colorRect[2] / 2)
+        cy = colorRect[1] + (colorRect[2] / 2)
+        frameSmooth = cv2.medianBlur(frame,7);
+        frameHSV = cv2.cvtColor(frameSmooth,cv2.cv.CV_BGR2HSV)
+        ch = frameHSV[cy][cx][0]
+        cs = frameHSV[cy][cx][1]
+        cv = frameHSV[cy][cx][2]
+
+        if ch >= 15 and cs >= 50 and cs <= 255 and cv >= 50 and cv <= 255:
+            lastRect.append([colorRect[0],colorRect[1],colorRect[2],colorRect[3]])
+
+    if len(lastRect) > 0:
+        for handRect in lastRect:
             cv2.rectangle(frame, tuple((handRect[0],handRect[1])),tuple((handRect[0]+handRect[2],handRect[1]+handRect[2])), color, thickness=2)
     
     display(frame)
