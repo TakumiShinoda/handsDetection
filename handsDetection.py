477df235e5e53732e00256b39353544954b1c54e
TakumiShinoda <takumishinoda@TakumiShinodanoMacBook-Pro.local>
2018-04-19 09:16:07 +0900

    fix for python's stable version

--- handsDetection.py
+++ handsDetection.py
@@ -1,23 +1,23 @@
 import cv2
 
-cascade_path = "/Users/macuser/Documents/Python/openCV/handsDetection/xml/cascade.xml"
+cascade_path = "xml/cascade.xml"
 cascade = cv2.CascadeClassifier(cascade_path)
 color = (255, 255, 255)
 cap = cv2.VideoCapture(0)
-minsize = 100
+minsize = 400
 
 def display(frame):
     cv2.imshow("frame",frame)
 
 while True:
     ret,frame = cap.read()
-    image_gray = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
+    image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(minsize,minsize))
 
     if len(rect) > 0:
         for handRect in rect:
             cv2.rectangle(frame, tuple((handRect[0],handRect[1])),tuple((handRect[0]+handRect[2],handRect[1]+handRect[2])), color, thickness=2)
-    
+
     display(frame)
-            
+
     cv2.waitKey(1)
